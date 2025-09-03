32850496c755079b030bf28a3d7cb821e8484340
Stephanie Gonzalez <xystar25@gmail.com>
2025-09-03 14:26:10 -0700

    Done

---
+++ sketch.js
@@ -0,0 +1,243 @@
+function setup() {
+    createCanvas(400, 400);
+}
+
+function draw() {
+    background(0);
+
+//Stars Circle Top
+    fill(255);
+    ellipse(200, 120, 80, 80);
+
+    fill(0);
+    ellipse(240, 77, 80, 80);
+    fill(0);
+    ellipse(160, 77, 80, 80);
+    fill(0);
+    ellipse(240, 157, 80, 80);
+    fill(0);
+    ellipse(160, 157, 80, 80);
+
+//Stars Circle Bottom
+    fill(255);
+    ellipse(200, 280, 80, 80);
+
+    fill(0);
+    ellipse(240, 323, 80, 80);
+    fill(0);
+    ellipse(160, 323, 80, 80);
+    fill(0);
+    ellipse(240, 243, 80, 80);
+    fill(0);
+    ellipse(160, 243, 80, 80);
+
+// Circle in stars
+    fill(0);
+    ellipse(200, 117, 25, 25); 
+    fill(0);
+    ellipse(200, 283, 25, 25);   
+
+    //Central circle
+    fill(255);
+    ellipse(200, 200, 50, 50);
+    fill(0);
+    ellipse(200, 200, 30, 30);
+
+    //Moon full Right
+    fill(255);
+    ellipse(250, 200, 40, 40);
+    fill(255);
+    ellipse(290, 200, 30, 30);
+    fill(255);
+    ellipse(320, 200, 20, 20);
+    fill(255);
+    ellipse(340, 200, 10, 10);
+    fill(255);
+    ellipse(352, 200, 5, 5);
+
+    //Moon full Left
+    fill(255);
+    ellipse(150, 200, 40, 40);
+    fill(255);
+    ellipse(110, 200, 30, 30);
+    fill(255);
+    ellipse(80, 200, 20, 20);
+    fill(255);
+    ellipse(60, 200, 10, 10);
+    fill(255);
+    ellipse(48, 200, 5, 5);
+
+    //Circle Top Left
+    fill(255);
+    ellipse(60, 60, 90, 90);
+
+    fill(0);
+    ellipse(105, 55, 75, 75);
+    fill(0);
+    ellipse(60, 105, 75, 75);
+
+    //Circle Top Right
+    fill(255);
+    ellipse(340, 60, 90, 90);
+
+    fill(0);
+    ellipse(295, 55, 75, 75);
+    fill(0);
+    ellipse(340, 105, 75, 75);
+
+    //Circle Bottom Left
+    fill(255);
+    ellipse(60, 340, 90, 90);
+
+    fill(0);
+    ellipse(105, 345, 75, 75);
+    fill(0);
+    ellipse(60, 300, 75, 75);
+
+    //Circle Bottom Right
+    fill(255);
+    ellipse(340, 340, 90, 90);
+
+    fill(0);
+    ellipse(295, 345, 75, 75);
+    fill(0);
+    ellipse(340, 300, 75, 75);
+
+    //stars :3
+    fill(255);
+    ellipse(100, 100, 5, 60);
+    noStroke();
+    fill(255);
+    ellipse(100, 100, 60, 5);
+
+    fill(255);
+    ellipse(100, 300, 5, 60);
+    noStroke();
+    fill(255);
+    ellipse(100, 300, 60, 5);
+    
+    fill(255);
+    ellipse(300, 300, 5, 60);
+    noStroke();
+    fill(255);
+    ellipse(300, 300, 60, 5);
+    
+    fill(255);
+    ellipse(300, 100, 5, 60);
+    noStroke();
+    fill(255);
+    ellipse(300, 100, 60, 5);
+
+
+
+    fill(255);
+    ellipse(200, 350, 5, 30);
+    noStroke();
+    fill(255);
+    ellipse(200, 350, 30, 5);
+
+    fill(255);
+    ellipse(200, 50, 5, 30);
+    noStroke();
+    fill(255);
+    ellipse(200, 50, 30, 5);
+
+    //blehh
+
+    fill(255);
+    ellipse(50, 255, 40, 40);
+
+    fill(0);
+    ellipse(30, 235, 40, 40);
+    fill(0);
+    ellipse(70, 235, 40, 40);
+    fill(0);
+    ellipse(70, 275, 40, 40);
+    fill(0);
+    ellipse(30, 275, 40, 40);
+
+    fill(255);
+    ellipse(350, 255, 40, 40);
+
+    fill(0);
+    ellipse(330, 235, 40, 40);
+    fill(0);
+    ellipse(370, 235, 40, 40);
+    fill(0);
+    ellipse(370, 275, 40, 40);
+    fill(0);
+    ellipse(330, 275, 40, 40);
+
+    fill(255);
+    ellipse(350, 140, 40, 40);
+
+    fill(0);
+    ellipse(330, 120, 40, 40);
+    fill(0);
+    ellipse(370, 120, 40, 40);
+    fill(0);
+    ellipse(370, 160, 40, 40);
+    fill(0);
+    ellipse(330, 160, 40, 40);
+
+    fill(255);
+    ellipse(50, 140, 40, 40);
+
+    fill(0);
+    ellipse(30, 120, 40, 40);
+    fill(0);
+    ellipse(70, 120, 40, 40);
+    fill(0);
+    ellipse(70, 160, 40, 40);
+    fill(0);
+    ellipse(30, 160, 40, 40);
+
+
+
+    fill(255);
+    ellipse(200, 200, 20, 20);
+    fill(0);
+    ellipse(200, 200, 15, 15);
+    fill(255);
+    ellipse(200, 200, 5, 5);
+
+    fill(0);
+    ellipse(247, 200, 28, 28);
+    fill(255);
+    ellipse(247, 200, 20, 20);
+    fill(0);
+    ellipse(247, 200, 15, 15);
+    fill(255);
+    ellipse(247, 200, 5, 5);
+    
+    fill(0);
+    ellipse(153, 200, 28, 28);
+    fill(255);
+    ellipse(153, 200, 20, 20);
+    fill(0);
+    ellipse(153, 200, 15, 15);
+    fill(255);
+    ellipse(153, 200, 5, 5);
+
+
+    fill(0);
+    ellipse(287, 200, 21, 21);
+    fill(255);
+    ellipse(287, 200, 13, 13);
+    fill(0);
+    ellipse(287, 200, 9, 9);
+    fill(255);
+    ellipse(287, 200, 3, 3);
+    
+    fill(0);
+    ellipse(113, 200, 21, 21);
+    fill(255);
+    ellipse(113, 200, 13, 13);
+    fill(0);
+    ellipse(113, 200, 9, 9);
+    fill(255);
+    ellipse(113, 200, 3, 3);
+
+
+
+}
